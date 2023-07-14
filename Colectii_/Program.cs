978ee684d162edf540d8bf5688746921055685aa
Ovidiu Gusteritan <gusteritanovidiu@gmail.com>
2023-07-14 23:54:30 +0300

    controler ChainedHashtable done

--- Colectii_/Program.cs
+++ Colectii_/Program.cs
@@ -14,24 +14,14 @@ class Program
     static void Main(string[] args)
     {
 
-        Persoana a = new Persoana("alex", 13, false);
-        Persoana b = new Persoana("mihai", 45, true);
-        Persoana c = new Persoana("vlad", 29, true);
+       ControlerChainedHashTable c=new ControlerChainedHashTable();
 
-        Programare p1 = new Programare("Dct 1", "mihai", "adr1", new DateTime(2020, 3, 23), new DateTime(2020, 3, 20));
-        Programare p2 = new Programare("Dct 2", "alex", "adr1", new DateTime(2019, 10, 2), new DateTime(2020, 10, 6));
-        Programare p3 = new Programare("Dct 3", "vlad", "adr1", new DateTime(2021, 5, 28), new DateTime(2020, 5, 30));
+        Programare p1 = new Programare("Dct 3", "vlad", "adr3", new DateTime(2021, 5, 28), new DateTime(2020, 5, 30));
+        Programare p2 = new Programare("Dct 3", "vlad", "sadasfasd", new DateTime(2011, 5, 28), new DateTime(2011, 5, 30));
 
+        c.update(p1, p2);
 
-        IHashTable<Persoana, Programare> chain = new ChainedHashtable<Persoana, Programare>(10);
-
-        chain.put(a, p2);
-        chain.put(b, p1);
-        chain.put(c, p3);
-
-        ILista<Persoana> lista=chain.keys();
-
-        lista.afisare();
+        c.afisare();
 
     }
 }
